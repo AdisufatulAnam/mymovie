88ec80314f035562a9f5b05f65f6c8b00da07b5b
nurzaman-now <imannj69@gmail.com>
2022-11-18 13:39:01 +0700

    fix keyword search null

--- src/script/data/data.js
+++ src/script/data/data.js
@@ -2,7 +2,12 @@ const url = "https://api.themoviedb.org/3";
 const api_key = "19a772b732ae958ca4e3c3fead70bc19";
 class DataSource {
   static async searchMovie(keyword) {
-    const response = await fetch(`${url}/search/movie?api_key=${api_key}&language=en-US&query=${keyword}&page=1&include_adult=false`);
+    let response;
+    if (keyword) {
+      response = await fetch(`${url}/search/movie?api_key=${api_key}&language=en-US&query=${keyword}&page=1&include_adult=false`);
+    } else {
+      response = await fetch(`${url}/movie/popular?api_key=19a772b732ae958ca4e3c3fead70bc19&page=1`)
+    }
     const responseJson = await response.json();
     if (responseJson.results) {
       return Promise.resolve(responseJson.results);
